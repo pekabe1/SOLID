58fabbeaad4c3bc9dce3866a2d0e6c05c0c2493c
k.ziarkowski <k.ziarkowski@PK1-3>
2019-10-22 18:49:17 +0200

    Gołomp pocztowy

---
+++ SOLID/D/SOLID_D.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID.D
+{
+    public class Messenger
+
+    {
+
+        //    public string SendMessages()
+
+        //    {
+
+        //        Sms sms = new Sms();
+
+        //        Email email = new Email();
+
+        //        Mms mms = new Mms();
+
+        //        return $"Wysyłam widomość za pomocą: {sms.SendSMS()}; {email.SendEmal()}; {mms.SendMMS()}";
+
+        //    }
+
+     }
+
+        public class Sms : IMessage
+
+        {
+            public string Send(string message)
+            {
+                return message + " sms ";
+            }
+        }
+
+        public class Email : IMessage
+
+        {
+            public string Send(string message)
+            {
+                return message + " email ";
+            }
+        }
+
+        public class Mms : IMessage
+
+        {
+            public string Send(string message)
+            {
+                return message + " mms ";
+            }
+        }
+
+
+    public class gołomp : IMessage
+
+    {
+        public string Send(string message)
+        {
+            return message + " gołomp ";
+        }
+    }
+}
+
--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -1,39 +1,55 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using SOLID.D;
 namespace SOLID
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            IPayment payment = null;
-            string input = Console.ReadLine();
+            //Console.WriteLine("Hello World!");
+            //IPayment payment = null;
+            //string input = Console.ReadLine();
 
-            switch (input)
-            {
-                case "cash":
-                    payment = new Cash();
-                        break;
-                case "card":
-                    payment = new CreditCard();
-                    break;
-                case "blik":
-                    payment = new BlikL();
-                    break;
-            }
-            try
-            {
-                payment.MakePayment(15);
-            }
-            catch (Exception ex)
-            {
+            //switch (input)
+            //{
+            //    case "cash":
+            //        payment = new Cash();
+            //            break;
+            //    case "card":
+            //        payment = new CreditCard();
+            //        break;
+            //    case "blik":
+            //        payment = new BlikL();
+            //        break;
+            //}
+            //try
+            //{
+            //    payment.MakePayment(15);
+            //}
+            //catch (Exception ex)
+            //{
 
-                Console.WriteLine(ex.Message);
+            //    Console.WriteLine(ex.Message);
+            //}
+            //Console.ReadLine();
+
+
+
+            List<IMessage> messages = new List<IMessage>();
+
+            messages.Add(new Sms());
+            messages.Add(new Email());
+            messages.Add(new Mms());
+
+            foreach (IMessage item in messages)
+            {
+                item.Send("message");
+                Console.WriteLine(item.Send("message"));
             }
             Console.ReadLine();
-            
 
+            
 
         }
     }
